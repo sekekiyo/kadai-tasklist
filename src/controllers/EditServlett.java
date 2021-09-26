59f400f113e2e155951526ac360db100fda56432
kiyoshi.yuda <suketa.kiyo@gmail.com>
2021-09-26 18:50:13 +0900

    EditServlet.javaを作成

---
+++ src/controllers/EditServlett.java
@@ -0,0 +1,44 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class EditServlett
+ */
+@WebServlet("/edit")
+public class EditServlett extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public EditServlett() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        Task t = em.find(Task.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        request.setAttribute("task", t);
+        request.setAttribute("_token", request.getSession().getId());
+
+        request.getSession().setAttribute("task_id", t.getId());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
+        rd.forward(request, response);
+    }
+
+}
