3eae9914fd03329af144b9fe0eb82a1a941a24bd
kiyoshi.yuda <suketa.kiyo@gmail.com>
2021-09-26 18:51:48 +0900

    Index/NewServletの修正

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -4,13 +4,14 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import models.Message;
+import models.Task;
 import utils.DBUtil;
 
 /**
@@ -20,6 +21,7 @@ import utils.DBUtil;
 public class IndexServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
+
     public IndexServlet() {
         super();
         // TODO Auto-generated constructor stub
@@ -28,10 +30,15 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        List<Message> tasks = em.createNamedQuery("getAllMessages", Message.class).getResultList();
+        List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class).getResultList();
         response.getWriter().append(Integer.valueOf(tasks.size()).toString());
 
         em.close();
+
+        request.setAttribute("tasks", tasks);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
+        rd.forward(request, response);
     }
 
 }
