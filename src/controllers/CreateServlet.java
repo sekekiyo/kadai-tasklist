370711cd8a7f1c2abbfb565d916db3b929897a51
kiyoshi.yuda <suketa.kiyo@gmail.com>
2021-09-29 14:53:46 +0900

    CreateServlet.javaを作成

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -21,8 +21,7 @@ public class CreateServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     public CreateServlet() {
-        super();
-        // TODO Auto-generated constructor stub
+        super();// TODO Auto-generated constructor stub
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
